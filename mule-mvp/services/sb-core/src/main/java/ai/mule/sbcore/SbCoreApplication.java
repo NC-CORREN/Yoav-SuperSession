a9c5edc25b26aa1c45f895edcb9a189e1c5dbcf4
NC-CORREN <n.cohen@corren.ch>
2025-11-19 16:46:22 +0100

    Create SbCoreApplication.java

---
+++ mule-mvp/services/sb-core/src/main/java/ai/mule/sbcore/SbCoreApplication.java
@@ -0,0 +1,12 @@
+package ai.mule.sbcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbCoreApplication.class, args);
+    }
+}
